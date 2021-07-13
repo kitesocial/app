472da91afcc7ef3234b8f1fe2c58326ca857e249
Samuel Cochran <sj26@sj26.com>
2021-07-13 16:01:51 +1000

    Switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_06_042431) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chirps", force: :cascade do |t|
     t.integer "author_id", null: false
     t.text "content"
